481fa6d75eb93a462d6636c4264bd9dfaebfb1e7
Samuel Gesang <sgesang@mailo.com>
2024-04-19 15:50:34 +0200

    add FigletFont

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+
 public class Main {
 
-public static void main(String[] args){
+public static void main(String[] args) throws IOException {
 
-System.out.println("Hello, world");
+System.out.println(FigletFont.convertOneLine("Hello, world!"));
+
 
 }
 
